f41576e6ad512144a34132d366f42bf76e7df415
Amos Folz <amosfolz@gmail.com>
2019-05-28 02:35:39 +0000

    initial commit for advanced tutorial additions

---
+++ src/Sprunje/PastriesSprunje.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * UserFrosting (http://www.userfrosting.com)
+ *
+ * @link      https://github.com/userfrosting/UserFrosting
+ * @copyright Copyright (c) 2019 Alexander Weissman
+ * @license   https://github.com/userfrosting/UserFrosting/blob/master/LICENSE.md (MIT License)
+ */
+
+namespace UserFrosting\Sprinkle\Pastries\Sprunje;
+
+use UserFrosting\Sprinkle\Core\Sprunje\Sprunje;
+use UserFrosting\Sprinkle\Pastires\Database\Models\Pastry;
+
+class PastriesSprunje extends Sprunje
+{
+    protected $name = 'pastries';
+
+    protected $sortable = [
+    'name',
+    'description',
+    'origin'
+    ];
+
+    protected $filterable = [
+    'name',
+    'description',
+    'origin'
+    ];
+
+    protected function baseQuery()
+    {
+        $instance = new Pastry();
+
+        return $instance->newQuery();
+    }
+}
