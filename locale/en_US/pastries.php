503c3f3e33835f6863a2519e7b9696cb47e98741
Amos Folz <amosfolz@gmail.com>
2019-06-01 14:03:57 +0000

    updates

--- locale/en_US/pastries.php
+++ locale/en_US/pastries.php
@@ -1,12 +1,15 @@
 <?php
 
 return [
-    "PASTRIES" => [
-        "@TRANSLATION" => "Pastries",
-        "PAGE" => "This page provides a yummy list of pastries",
-        "LIST" => "List of Pastries",
-        "NAME" => "Name",
-        "ORIGIN" => "Origin",
-        "DESCRIPTION" => "Description"
+    'PASTRIES' => [
+        '@TRANSLATION' => 'Pastries',
+        'PAGE'         => 'This page provides a yummy list of pastries',
+        'LIST'         => 'List of Pastries',
+        'NAME'         => 'Name',
+        'ORIGIN'       => 'Origin',
+        'DESCRIPTION'  => 'Description',
+        'CREATE'       => 'Create pastry',
+        'EDIT'         => 'Edit pastry',
+        'DELETE'       => 'Delete pastry'
     ]
 ];
