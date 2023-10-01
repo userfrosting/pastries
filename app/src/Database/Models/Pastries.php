9d109de3b85f0eaca9dba57c307a069371893dd7
Louis Charette <charette.louis@gmail.com>
2023-09-30 21:53:38 -0400

    Remove table name (redundant, not required)

--- app/src/Database/Models/Pastries.php
+++ app/src/Database/Models/Pastries.php
@@ -14,11 +14,6 @@ use UserFrosting\Sprinkle\Core\Database\Models\Model;
 
 class Pastries extends Model
 {
-    /**
-     * @var string The name of the table for the current model.
-     */
-    protected $table = 'pastries';
-
     protected $fillable = [
         'name',
         'description',
